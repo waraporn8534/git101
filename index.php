2a713295310c02e9f9a0f8cca3a6b7303139748c
waraporn <buttakarm@gmail.com>
2017-09-24 11:48:27 +0700

    commit1

---
+++ index.php
@@ -0,0 +1,40 @@
+<?
+include "head.php";
+include "config.php";
+?>
+    <body>
+        <div class="container">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    ส่วนหัว
+                </div>
+                <div class="panel-body">
+                    Basic panel example
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <h2>
+                                        <?echo $system_name; ?>
+                                    </h2>
+                                    <h3>
+                                        <?echo $school_name; ?>
+                                    </h3>
+                                    <p>
+                                        <a class="btn btn-primary btn-lg" href="login.php">
+                                            เข้าระบบ
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+ <?
+include("footer.php");
+ ?>
+    </body>
+</html>
